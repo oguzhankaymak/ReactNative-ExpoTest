41a9f3f7ba20e5b0fc98bd4fe5be3bd385f6f0dc
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-02-11 22:23:51 +0300

    add tests structure

---
+++ src/screens/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <View style={{ paddingTop: 40 }}>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
