bb8e4dcdf10e2c235a18185103f73bf97907efa3
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-02-13 10:34:24 +0300

    add product in cart

--- src/screens/electronicsScreen/ElectronicsScreen.js
+++ src/screens/electronicsScreen/ElectronicsScreen.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { View } from "react-native";
+import { useDispatch } from "react-redux";
 
+import CartItemActions from "../../redux/CartItemRedux";
 import { electronics } from "../../../Data";
 import Product from "../../components/product/Product";
 
 const ElectronicsScreen = ({ navigation }) => {
+  const dispatch = useDispatch();
   return (
     <View style={{ flex: 1 }}>
-      <Product products={electronics} />
+      <Product
+        products={electronics}
+        onPressBtn={(product) => dispatch(CartItemActions.AddToCart(product))}
+      />
     </View>
   );
 };
