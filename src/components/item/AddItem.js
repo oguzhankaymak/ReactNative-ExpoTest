3cbd120428b480a1f5d68731e180e66121250c05
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-02-14 16:26:28 +0300

    refactor note list screen and item component

--- src/components/item/AddItem.js
+++ src/components/item/AddItem.js
@@ -1,25 +1,20 @@
-import React, { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
-import styles from "./styles/AddItemStyle";
+import styles from './styles/AddItemStyle';
 
 const AddItem = ({ addItem }) => {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState('');
   return (
     <View style={styles.item}>
-      <TextInput
-        style={styles.textInput}
-        onChangeText={setValue}
-        value={value}
-        testID={"add-input"}
-      />
+      <TextInput style={styles.textInput} onChangeText={setValue} value={value} testID={'add-input'} />
       <TouchableOpacity
         style={styles.roundPlus}
-        testID={"add-button"}
-        onPress={() =>
-          addItem({ id: (Math.random() * 1000000).toString(), value: value })
-        }
-      >
+        testID={'add-button'}
+        onPress={() => {
+          addItem({ id: (Math.random() * 1000000).toString(), value: value });
+          setValue('');
+        }}>
         <Text style={styles.plus}>+</Text>
       </TouchableOpacity>
     </View>
