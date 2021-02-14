8fcf8b15ef873fb659c086634ae1d3c9a0b19a08
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-02-14 19:08:53 +0300

    refactor cart screen

---
+++ src/screens/cartScreen/styles/CartScreenStyle.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+
+  card: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    margin: 20,
+    padding: 15,
+    width: 200,
+    backgroundColor: '#006d77',
+    borderRadius: 40,
+  },
+
+  text: {
+    margin: 5,
+    fontSize: 13,
+    color: 'white',
+  },
+});
