6aa1d29b7b13d3df00f364ec9bd97104b5968056
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-02-14 21:49:23 +0300

    test add products in redux

--- src/screens/electronicsScreen/ElectronicsScreen.js
+++ src/screens/electronicsScreen/ElectronicsScreen.js
@@ -8,7 +8,7 @@ import Product from '../../components/product/Product';
 import Layout from '../../components/layout/Layout';
 import styles from './styles/ElectronicsScreenStyle';
 
-const ElectronicsScreen = ({ navigation }) => {
+const ElectronicsScreen = () => {
   const dispatch = useDispatch();
   return (
     <Layout>
