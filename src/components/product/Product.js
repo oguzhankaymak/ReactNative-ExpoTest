d6827799718d0d9de248e77e0708f92a3e58b0c8
Oguzhan Kaymak <oguzhankaymakdev@gmail.com>
2021-02-14 16:42:34 +0300

    refactor electronics screen and product component

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,18 +1,15 @@
-import React from "react";
-import { Text, TouchableOpacity } from "react-native";
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+
+import styles from './styles/ProductStyle';
 
 const Product = ({ products, onPressBtn }) => {
   return products.map((item, index) => {
     return (
-      <TouchableOpacity
-        onPress={() => onPressBtn(item)}
-        key={String(index)}
-        style={{
-          padding: 20,
-          alignItems: "center",
-        }}
-      >
-        <Text>{item.name + " - " + item.price}</Text>
+      <TouchableOpacity onPress={() => onPressBtn(item)} key={String(index)} style={styles.card}>
+        <Text>{item.name + ' - ' + item.price}</Text>
+        <AntDesign name="pluscircleo" size={24} color="#fff" />
       </TouchableOpacity>
     );
   });
